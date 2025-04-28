986efdcec8eb4874f3283e0ee76d3614d9b76a42
Billy Aguinaga <aguinaga.b@gmail.com>
2025-04-28 01:35:05 -0400

    bugfix: added typing for pagesArray in NavMenu

--- src/app/components/shared/nav-menu/nav-menu.component.ts
+++ src/app/components/shared/nav-menu/nav-menu.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 
+type PagesArray = { label: string; url: string }[];
+
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css',
 })
 export class NavMenuComponent {
-  @Input() pages: string[] = [];
+  @Input() pages: PagesArray = [];
 }
