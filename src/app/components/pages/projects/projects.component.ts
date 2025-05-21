acfdf500bff7979c69fc752d223c70ff40218918
Billy Aguinaga <aguinaga.b@gmail.com>
2025-05-21 16:58:52 -0400

    refactor:moved projects data to .json file, uploading response sample

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -1,54 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
   isCurrentOpen = true;
   isPastOpen = false;
 
-  currentProjects = [
-    {
-      name: 'Project 1',
-      description: 'A brief description of Project 1.',
-      technologies: ['Angular', 'TailwindCSS', 'Firebase'],
-      githubLink: 'https://github.com/username/project1',
-      demoLink: 'https://project1.com',
-      image: 'assets/images/project1.jpg',
-    },
-    {
-      name: 'Project 2',
-      description: 'A brief description of Project 2.',
-      technologies: ['React', 'Node.js', 'MongoDB'],
-      githubLink: 'https://github.com/username/project2',
-      demoLink: null, // No demo link for this project
-      image: 'assets/images/project2.jpg', // No image for this project
-    },
-  ];
+  currentProjects: any[] = [];
+  pastProjects: any[] = [];
+  placeholderImage = 'assets/images/placeholder.png';
 
-  pastProjects = [
-    {
-      name: 'Project 3',
-      description: 'A brief description of Project 3.',
-      technologies: ['Vue.js', 'Express', 'PostgreSQL'],
-      githubLink: 'https://github.com/username/project3',
-      demoLink: 'https://project3.com',
-      image: 'assets/images/project3.jpg',
-    },
-    {
-      name: 'Project 4',
-      description: 'A brief description of Project 4.',
-      technologies: ['Django', 'Python', 'SQLite'],
-      githubLink: 'https://github.com/username/project4',
-      demoLink: null, // No demo link for this project
-      image: null, // No image for this project
-    },
-  ];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadProjects();
+  }
+
+  loadProjects(): void {
+    this.http.get('assets/projects.json').subscribe((data: any) => {
+      this.currentProjects = data.currentProjects;
+      this.pastProjects = data.pastProjects;
+    });
+  }
 
   toggleAccordion(section: 'current' | 'past'): void {
     if (section === 'current') {
